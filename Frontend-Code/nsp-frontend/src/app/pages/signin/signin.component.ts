93876a76aa175edfd03645fd34c28f03f27a730c
Paul-Blankson <paul.blankson@amalitech.com>
2025-02-19 18:13:04 +0000

    feat: add signin component with headline and card

--- Frontend-Code/nsp-frontend/src/app/pages/signin/signin.component.ts
+++ Frontend-Code/nsp-frontend/src/app/pages/signin/signin.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { InputFieldComponent } from "../../shared/input-field/input-field.component";
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [InputFieldComponent],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
