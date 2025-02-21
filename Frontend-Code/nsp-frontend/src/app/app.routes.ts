1aff61adc1c3b6587330393c9dcb535e6a9fdec9
Paul-Blankson <paul.blankson@amalitech.com>
2025-02-21 17:21:57 +0000

    feat: add route for reset password

--- Frontend-Code/nsp-frontend/src/app/app.routes.ts
+++ Frontend-Code/nsp-frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './pages/signin/signin.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/signin', pathMatch: 'full' },
-  { path: 'signin', title: 'Signin', component: SigninComponent }
+  { path: 'signin', title: 'Signin', component: SigninComponent },
+  { path: 'reset-password', title: 'Reset Password', component: ResetPasswordComponent }
 ];
